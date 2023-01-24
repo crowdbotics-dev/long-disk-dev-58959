164cfdcc7287d18064f48a85dce03ae251e3e0ad
Crowdbotics <engineering@crowdbotics.com>
2023-01-24 00:15:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "long-disk-dev-58959",
+  url: "https://long-disk-dev-58959.botics.co",
+  api: "https://long-disk-dev-58959.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
